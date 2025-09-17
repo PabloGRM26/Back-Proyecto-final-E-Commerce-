2556fa50ec378bd9e3bb3a7b9bf71eba104c1f78
PabloGRM26 <pablo.rodriguez@anima.edu.uy>
2025-09-17 19:38:53 -0300

    user update

--- Proyecto_E-Commerce/Proyecto Base/models/User.js
+++ Proyecto_E-Commerce/Proyecto Base/models/User.js
@@ -1,5 +1,35 @@
 const { Model, DataTypes } = require("sequelize");
 
+class Admin extends Model {
+  static initModel(sequelize) {
+    Admin.init(
+      {
+        id: {
+          type: DataTypes.BIGINT.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        username: {
+          type: DataTypes.STRING,
+          unique: true,
+        },
+        email: {
+          type: DataTypes.STRING,
+          unique: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        sequelize,
+        modelName: "admin",
+      },
+    );
+    return Admin;
+  }
+}
+
 class User extends Model {
   static initModel(sequelize) {
     User.init(
@@ -15,14 +45,30 @@ class User extends Model {
         lastname: {
           type: DataTypes.STRING,
         },
+        password: {
+          type: DataTypes.STRING,
+        },
+        adress: {
+          type: DataTypes.STRING,
+        },
+        telephone: {
+          type: DataTypes.STRING,
+        },
+        wishlist: {
+          type: DataTypes.ARRAY(DataTypes.BIGINT.UNSIGNED),
+        },
+        email: {
+          type: DataTypes.STRING,
+          unique: true,
+        },
       },
       {
         sequelize,
-        modelName: "user", // Nombre del modelo en singular y en minúscula.
+        modelName: "user",
       },
     );
     return User;
   }
 }
 
-module.exports = User;
+module.exports = { User, Admin };
