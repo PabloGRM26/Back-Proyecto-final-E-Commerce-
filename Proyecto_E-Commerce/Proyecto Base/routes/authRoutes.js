7f3b00e23a5cdb2cfd5f8d0cda7158d2b7e82444
BrunoMartinez17 <Bruno.Martinez@anima.edu.uy>
2025-10-08 21:02:44 -0300

    Update

--- Proyecto_E-Commerce/Proyecto Base/routes/authRoutes.js
+++ Proyecto_E-Commerce/Proyecto Base/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middlewares/VerifyToken");
 const authCtrl = require("../controllers/authController");
 
 // Ruta de login
 router.post("/login", authCtrl.login);
 
 // Ejemplo de ruta protegida
-router.get("/perfil", authCtrl.verifyToken, (req, res) => {
+router.get("/perfil", verifyToken.verifyToken, (req, res) => {
   res.json({ message: `Bienvenido, ${req.user.email}` });
 });
 
