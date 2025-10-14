7a005d3acfb297dbe4907e22bbf82b39924cee21
BrunoMartinez17 <Bruno.Martinez@anima.edu.uy>
2025-10-13 21:26:03 -0300

    Update

--- Proyecto_E-Commerce/Proyecto Base/server.js
+++ Proyecto_E-Commerce/Proyecto Base/server.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const routes = require("./routes");
 
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
+
+// 🟢 CORS: permitimos al front conectarse
+app.use(
+  cors({
+    origin: "http://localhost:5173", // dirección del frontend (Vite)
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
 
 app.use(express.json());
 app.use(express.static("public"));
